56638712e9fa69e15d5bbdd376bb2bf09b38293f
Ashish Rawat <arawat0429@gmail.com>
2026-03-17 21:16:47 +0530

    second inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './index.css'
+import { SparklesPreview } from './components/ui/SparklesPreview'
 
 function App() {
   return (
     <div className='h-screen w-full bg-zinc-800 text-white'>
       <h1 className='text-3xl font-bold text-center mt-10'>Hello World</h1>
       <p className='text-center mt-4'>This is a React app with Tailwind CSS.</p>
+      <SparklesPreview />
     </div>
   )
 }
