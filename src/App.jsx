7b1373d910f1a39e0c3957bda81a5037af58e5a6
Ashish Rawat <arawat0429@gmail.com>
2026-03-17 20:04:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className='h-screen w-full bg-zinc-800 text-white'>
+      <h1 className='text-3xl font-bold text-center mt-10'>Hello World</h1>
+      <p className='text-center mt-4'>This is a React app with Tailwind CSS.</p>
+    </div>
+  )
+}
+
+export default App
